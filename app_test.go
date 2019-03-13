af8ab4d5796b7215c5d3de5fb0e933b88648dc07
Jay <dxumop@gmail.com>
2019-03-13 15:55:25 +0700

    newbranch2

--- app_test.go
+++ app_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestSum(t *testing.T) {
     total := Sum(40, 4)
-    if total != 44 {
+    if total != 33 {
        t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
     }
 }
